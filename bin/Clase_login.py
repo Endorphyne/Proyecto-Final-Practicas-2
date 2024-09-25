d7e090b6019d5dbd084a36d1cf34030c3b3e3c67
Endorphyne <ohorodeski@gmail>
2024-09-25 18:04:29 -0300

    Cambios mayores al login, inicio de sistema de historial

--- bin/Clase_login.py
+++ bin/Clase_login.py
@@ -1,40 +1,107 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
-# from REMPLAZAR_ARCHIVO import Ui_MainWindow  # Asegúrate de que este es el nombre correcto del archivo convertido
+from Clase_ventana import Ui_MainWindow  # Asegúrate de que este es el nombre correcto del archivo convertido
+from clase_admin import Ui_admin_mainWindow
+from clase_empleado import Ui_empleado_MainWindow
+from Clase_ventana_registro import Ui_ventana_registro
+import utils_loggin
+
+class admin_window(QMainWindow, Ui_admin_mainWindow):
+    def __init__(self) -> None:
+        super().__init__()  # Llamada correcta a super()
+        self.setupUi(self)
+
+class empleado_window(QMainWindow, Ui_empleado_MainWindow):
+    def __init__(self) -> None:
+        super().__init__()  # Llamada correcta a super()
+        self.setupUi(self)
+
 
 class Clase_login(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)  # Configura la interfaz
-        self.ok_btn.clicked.connect(self.mostrar_error)  # Conecta el botón con el método mostrar_error
+        self.btn_iniciar_sesion.clicked.connect(self.filtro_usuario)  # Conecta el botón con el método filtro_usua
+        self.btn_tegistrarse.clicked.connect(self.registro)
 
-    def mostrar_error(self):
-        msg = QMessageBox()
-        msg.setIcon(QMessageBox.Warning)
-        msg.setText("Datos de Usuario y/o Contraseña incorrectos.")
-        msg.setWindowTitle("Advertencia")
-        msg.setStandardButtons(QMessageBox.Ok)
-        # Aplicar estilo personalizado
-        msg.setStyleSheet("""
-            QMessageBox {
-                background-color: #232743;  /* Fondo general */
-                color: #797979;  /* Detalles */
-                font-size: 14px;
-            }
-            QMessageBox QLabel {
-                color: #D8DBE2;  /* Fondo específico */
-            }
-            QPushButton {
-                background-color: #5E7CE2;  /* Botones */
-                color: #ECEFF4; 
-                padding: 5px;
-                border-radius: 5px;
-            }
-            QPushButton:hover {
-                background-color: #81A1C1;
-            }
-        """)
-        msg.exec()
+    def filtro_usuario(self,) -> None:
+        """
+        Ejecuta una ventana u otra basándose en el grupo del id_usuario (1:empleado/0:admin)
+        """
+        id_usuario = utils_loggin.obtener_data(self.line_usuario.text())
+        grupo = id_usuario[4]
+        if grupo == 0:
+            self.admin_window = admin_window()
+            self.admin_window.show()
+            self.close()
+        else:
+            self.empleado_window = empleado_window()
+            self.empleado_window.show()
+            self.close()
+
+    def registro(self,) -> None:
+        """
+        mostrar la venata de registro
+        """
+        self.registro_window = registro_window()
+        self.registro_window.show()
+        self.close()
+
+class registro_window(QMainWindow, Ui_ventana_registro):
+    #TODO pasar a la aplicacion del admin
+    def __init__(self) -> None:
+        super().__init__()  # Llamada correcta a super()
+        self.setupUi(self)
+        self.btn_confirmar.clicked.connect(self.slot_usuarios)
+
+    def slot_usuarios(self):
+        utils_loggin.agregar_usuario(self.validar_datos_registro())
+    
+    def validar_datos_registro(self):
+        try:
+            if utils_loggin.validador_email(self.line_correo.text()) == True:
+                correo = self.line_correo.text()
+            else:
+                raise ValueError("Correo ingresado no valido")
+            print(self.line_usuario.text())
+            if utils_loggin.validar_usuario(self.line_usuario.text()) == True:
+                nombre_usuario = self.line_usuario.text()
+            password1 = self.line_pass1.text()
+            password2 = self.line_pass2.text()
+            if self.cmBox_admin.currentText() == "Cliente":
+                grupo = 2
+            elif self.cmBox_admin.currentText() == "Empleado":
+                grupo = 1
+            else:
+                grupo = 0
+            if password1 != password2:
+                raise ValueError("Las contraseñas no coinciden.")
+            password = password1
+        # mensajes de error para datos mas ingresados
+        except ValueError as e:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Warning)
+            msg.setText(str(e))
+            msg.setWindowTitle("Advertencia")
+            msg.setStandardButtons(QMessageBox.Ok)
+            msg.exec()
+            return None
+
+        except Exception as e:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Warning)
+            msg.setText("Error en los datos ingresados.")
+            msg.setWindowTitle("Advertencia")
+            msg.setStandardButtons(QMessageBox.Ok)
+            msg.exec()
+            return None
+
+        else:
+            user = utils_loggin.usuario(correo, nombre_usuario, password, grupo)
+            return user
+
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)  # Crea la aplicación
     window = Clase_login()  # Crea una instancia de la ventana principal
---
+++ bin/utils_loggin.py
@@ -0,0 +1,155 @@
+#TODO Antes de pasar a main cambiar las bases de datos de prueba a las finales
+from mysql.connector import MySQLConnection,Error
+from db_conection_config import conexion_db
+from PyQt5.QtWidgets import QMessageBox
+import bcrypt
+import re
+import session
+from datetime import datetime
+
+class usuario:
+    """
+    Clase que almacena los datos del usuario para uso interno y registro
+    """
+    def __init__(self,correo:str,usuario:str,password:str,grupo:int) -> None:
+        self.correo = correo
+        self.usuario = usuario
+        self.password = password
+        self.grupo = grupo
+
+def comprobar_credenciales(usuario:str,contra:str) -> bool:
+    '''
+    Obtiene el usuario recibido y revisa la contraseña recibida comparandola con la almacenada en la DB y devuelve un valor booleano
+    '''
+    flag = False
+    config_db = conexion_db("bin\config.ini","Proyecto Practicas2")
+    #conexion con la base de datos desempaquetando el config
+    conexion = MySQLConnection(**config_db)
+    #cursor para ejecutar las consultas SQL
+    cursor = conexion.cursor()
+    try:
+        cursor.execute("SELECT usser_id FROM usuarios WHERE usuario = %s",(usuario,))
+        resultado = cursor.fetchone()
+        if resultado:
+            id_usuario = resultado[0]
+            #obtener contraseña
+            cursor.execute("SELECT password FROM usuarios WHERE usser_id = %s",(id_usuario,))
+            resultado = cursor.fetchone()
+            if resultado:
+                passw_hasheada = resultado[0]
+                #validar la contraseña
+                if bcrypt.checkpw(contra.encode('utf-8'),passw_hasheada.encode('utf-8')):
+                    flag = True
+                else:
+                    msg = QMessageBox()
+                    msg.setIcon(QMessageBox.Warning)
+                    msg.setText("Datos de Usuario y/o Contraseña incorrectos.")
+                    msg.setWindowTitle("Advertencia")
+                    msg.setStandardButtons(QMessageBox.Ok)
+                    msg.exec()
+    except Error as err:
+        print(f"Error: {err}")
+    except:
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Warning)
+        msg.setText("Datos de Usuario y/o Contraseña incorrectos.")
+        msg.setWindowTitle("Advertencia")
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.exec()
+    finally:
+        cursor.close()
+        conexion.close()
+    return flag
+
+def agregar_usuario(usuario:usuario) -> None:
+    ''''
+    Recibe datos validados previamente para solo subirlos a la DB, no retorna ningun valor
+    '''
+    config_db = conexion_db("bin\config.ini","Proyecto Practicas2")
+    conexion = MySQLConnection(**config_db)
+    cursor = conexion.cursor()
+    hashpw = bcrypt.hashpw((usuario.password.encode('utf-8')),bcrypt.gensalt()).decode('utf-8')
+    consultaSQL = "INSERT INTO usuarios (usuario,password,correo,grupo,fecha_creacion,ult_modificacion) VALUES (%s,%s,%s,%s,%s,%s)"
+    values = (usuario.usuario,hashpw,usuario.correo,usuario.grupo,datetime.now(),datetime.now())
+    try: 
+        cursor.execute(consultaSQL,values)
+        conexion.commit()
+    except Error as err:
+        print(f"Error: {err}")
+    finally:
+        cursor.close()
+        conexion.close()
+
+def obtener_data(usuario:str)->list:
+    """"
+    Obtiene el id de un usuario en base a un username
+    """
+    config_db = conexion_db("bin\config.ini","Proyecto Practicas2")
+    #conexion con la base de datos desempaquetando el config
+    conexion = MySQLConnection(**config_db)
+    #cursor para ejecutar las consultas SQL
+    cursor = conexion.cursor()
+    try:
+        cursor.execute("SELECT * FROM usuarios WHERE usuario = %s",(usuario,))
+        resultado = cursor.fetchone()
+        return resultado
+    except Error as err:
+        print(f"Error: {err}")
+
+def hashear(contra:str)->str:
+    return bcrypt.hashpw((contra.encode('utf-8')),bcrypt.gensalt()).decode('utf-8')
+
+#TODO's 
+'''
+Realizar la conexion logica con las ventanas SQL, Funciones 2 y 3 requieren los datos previos validados antes de pasarlos, ver si mejoraralo.
+'''
+
+def validar_usuario(usuario:str) -> bool:
+    """
+    Levanta un error por cuenta propia y devuelve un booleano
+    """
+    #coneccion con la DB
+    config_db = conexion_db("bin\config.ini","Proyecto Practicas2")
+    conexion = MySQLConnection(**config_db)
+    cursor = conexion.cursor()
+    try:
+        #consulta para obtener un valor en caso de que el usuario exista
+        cursor.execute("SELECT usuario FROM usuarios WHERE usuario = %s",(usuario,))
+        resultado = cursor.fetchone()
+        if resultado is None:
+            return True
+        #return que devuelve true si no existe ya un usuario con ese nombre
+        else:
+            raise ValueError ("El usuario ya existe")
+        
+    except ValueError as e:
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Warning)
+        msg.setText(str(e))
+        msg.setWindowTitle("Advertencia")
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.exec()
+        return False
+    #mensaje de error por si existe el usuario
+    except:
+        print("Ni idea que se rompio")
+
+def validador_email(email) -> bool:
+    """
+    Funcion para validar un email (muy basico sin APIs solamente formato), usar el booleano para validar
+    """
+    patron = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    return re.match(patron, email) is not None
+
+def ult_vez_mod(id_usuario:int, accion:str) -> None:
+    """
+    Funcion que modifica la ultima vez de un usuario y registra su accion
+    """
+    #TODO implementar logger
+    config_db = conexion_db("bin\config.ini","Proyecto Practicas2")
+    conexion = MySQLConnection(**config_db)
+    cursor = conexion.cursor()
+    try: 
+        consulta="INSERT INTO "
+    except:
+        pass
